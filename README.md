350be5e5a3ebed090a2c9730c1442ddeff2b0642
Recessive <aa.mindustry@gmail.com>
2020-03-09 16:23:19 +1100

    Update readme to include warning

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### GrayScale
 
 Removes individuality and makes for a very boring lobby.
+
+CURRENTLY PERMANENTLY SETS PLAYERS NAMES UNTIL THEY RESTART THEIR GAME DUE TO A BUG.
--- src/main/java/grayscale/GrayScale.java
+++ src/main/java/grayscale/GrayScale.java
@@ -3,17 +3,21 @@ package grayscale;
 
 import arc.Events;
 import arc.graphics.Color;
+import arc.util.Log;
 import mindustry.game.EventType;
 import mindustry.plugin.Plugin;
 import mindustry.entities.type.Player;
 
-public class GrayScale extends Plugin {
+import java.util.HashMap;
+import java.util.Map;
 
+public class GrayScale extends Plugin {
     @Override
     public void init(){
         Events.on(EventType.PlayerConnect.class, event -> {
             filterColor(event.player);
         });
+
     }
     public void filterColor(Player player){
         player.name = player.name.replaceAll("\\[(.*?)]","");
